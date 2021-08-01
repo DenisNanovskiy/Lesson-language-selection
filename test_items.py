eaa26e04b1e9f552aa4b768a6e3cf81950fe609c
DenisNanovskiy <56844278+DenisNanovskiy@users.noreply.github.com>
2021-08-01 21:02:17 +0300

    rename test

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 # import time
 
 
-def test_item(browser):
+def test_contains_an_add_to_cart_button(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     flag = False
     try:
