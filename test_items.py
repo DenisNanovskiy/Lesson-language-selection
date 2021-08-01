2f66c38d1d1a0d236aabe879909b07aca1c35fec
DenisNanovskiy <56844278+DenisNanovskiy@users.noreply.github.com>
2021-08-01 18:55:28 +0300

    Initial commit

---
+++ test_items.py
@@ -0,0 +1,12 @@
+# import time
+
+
+def test_item(browser):
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    flag = False
+    try:
+        button_submit = browser.find_element_by_css_selector('button.btn-add-to-basket')
+        flag = True
+    except:
+        assert flag is True, "No Such Element"
+    # time.sleep(30)
